9f7c05de76a498bcddb15f4a6c1995b7eb6127b7
nathancampos23 <126831025+nathancampos23@users.noreply.github.com>
2025-11-25 20:00:42 -0300

    Delete das-1-2025-2-Nathan/das-1-2025-2-a-main directory

--- das-1-2025-2-Nathan/das-1-2025-2-a-main/designpatterns/src/main/java/br/univille/observer/ConcreteSubscriber.java
+++
@@ -1,9 +0,0 @@
-package br.univille.observer;
-
-public class ConcreteSubscriber implements Subscriber {
-
-    @Override
-    public void update(String context) {
-        System.out.println(context);
-    }
-}
